471cc29953edaaef39ff8d8f0edc2ad664e4e90b
sarikarathod602-sketch <sarikarathod602@gmail.com>
2025-10-24 15:15:44 +0530

    Create Alert service.java

---
+++ Alert service.java
@@ -0,0 +1,9 @@
+package com.jit.warehouse;
+
+public class AlertService implements StockObserver {
+    @Override
+    public void onLowStock(Product product) {
+        System.out.println("⚠️ Restock Alert: Low stock for " + product.getName() + 
+                           " – only " + product.getQuantity() + " left!");
+    }
+}
