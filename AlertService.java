e71510ffa6d4007a60183c338985ced3db1ac01c
sarikarathod602-sketch <sarikarathod602@gmail.com>
2025-10-31 13:29:37 +0530

    Delete AlertService.java

--- AlertService.java
+++
@@ -1,8 +0,0 @@
-package com.jit.warehouse;
-
-public class AlertService implements StockObserver {
-    public void onLowStock(Product product) {
-        System.out.println("⚠️ Restock Alert: Low stock for " + product.getName() + 
-                           " – only " + product.getQuantity() + " left!");
-    }
-}
